1fc51750d1259e66787b1e7df923ed937b87ae38
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-07-17 19:02:52 +0530

    reverse url

--- backend/drfhome/product/serializers.py
+++ backend/drfhome/product/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
 from .models import Product
 
 class ProductSerializers(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
+            'url',
             'pk',
             'title',
             'content',
@@ -13,6 +17,13 @@ class ProductSerializers(serializers.ModelSerializer):
             'sale_price',
             'my_discount'
         ]
+
+    def get_url(self,obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-details",kwargs={"pk":obj.pk},request=request)
+
 
     def get_my_discount(self,obj):
         return obj.get_discount()
--- backend/drfhome/product/urls.py
+++ backend/drfhome/product/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # path('',views.PostAPIView.as_view()),
     path('',views.ListCreateAPIView.as_view()),
-    path('<int:pk>/',views.ProductAPIView.as_view()),
+    path('<int:pk>/',views.ProductAPIView.as_view(),name="product-details"),
     path('<int:pk>/update/',views.PutAPIView.as_view()),
     path('<int:pk>/delete/',views.DeleteAPIView.as_view()),
     # path('list/',views.ListAPIView.as_view()),
